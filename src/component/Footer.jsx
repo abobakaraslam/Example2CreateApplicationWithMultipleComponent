e1360bf197c17f86fc518f8ee98bea18ef1ef55e
abobakaraslam <164588250+abobakaraslam@users.noreply.github.com>
2024-05-29 07:52:46 +0500

    Add files via upload

---
+++ src/component/Footer.jsx
@@ -0,0 +1,16 @@
+// File: Header.jsx located in root-directory/src/component/
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      {/* container 6: Footer */}
+      <div className="text-center m-6">
+        <h1>Footer</h1>
+        <p>This is Footer container</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
